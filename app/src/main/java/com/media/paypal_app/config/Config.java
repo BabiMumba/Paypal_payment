ff96185c1b34b1ad0a96ecc3a4983601c1632286
Babi Mumba <babimumba243@gmail.com>
2022-08-04 11:16:44 +0200

    add a new file

---
+++ app/src/main/java/com/media/paypal_app/config/Config.java
@@ -0,0 +1,6 @@
+package com.media.paypal_app.config;
+
+public class Config {
+    public static final String PAYPAL_CLIENT_ID ="AX4RJLQKdPIFhYRQRsFPmZ6GVQjt2FN4XviLtUCa-6cQugQJAbPJPt_NtJOH9O2zuWwPKV1sGIfcGrSA";
+
+}
